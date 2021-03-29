7a8eef66713bab1919f6341ebd07ec63200f9d49
Asus <Asus@DESKTOP-SK2I7K7>
2021-03-29 16:03:56 +0300

    Добавьте файлы проекта.

---
+++ Aquarium.DAL/Repository/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aquarium.DAL.Repository
+{
+    public abstract class BaseRepository
+    {
+        protected readonly ApplicationDbContext dbContext;
+        protected BaseRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
